662d53190a7df6197c1a4a29fcc49afaf2aa347b
ZerosPen <andhikaatmaja51@gmail.com>
2026-09-09 15:30:05 +0700

    Fix Some Hieracy Order & Close Status btn

--- Assets/Script/Status Sytem/StatusPlayerFrame.cs
+++ Assets/Script/Status Sytem/StatusPlayerFrame.cs
@@ -12,12 +12,14 @@ public class StatusPlayerFrame : MonoBehaviour
         {
             _isOpen = true;
             canvasGroup.alpha = 1;
+            canvasGroup.interactable = true;
             canvasGroup.blocksRaycasts = true;
         }
         else
         {
             _isOpen = false;
             canvasGroup.alpha = 0;
+            canvasGroup.interactable = false;
             canvasGroup.blocksRaycasts = false;
         }
     }
